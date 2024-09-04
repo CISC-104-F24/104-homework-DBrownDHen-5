5a02b382c6add2f31d1e6fe3694a08b91828ae23
DBrownDHen <dbrownki@udel.edu>
2024-09-04 12:19:35 -0400

    completes Homework 0 Part C

--- Homework Project/Assets/Scripts/HelloWorld.cs
+++ Homework Project/Assets/Scripts/HelloWorld.cs
@@ -8,6 +8,14 @@ public class HelloWorld : MonoBehaviour
     void Start()
     {
         Debug.Log("Hello, World!");
+        Debug.LogWarning("Hello, I am a warning!");
+        Debug.LogError("Hello, I am an error!");
+        // Author: Dawson Brown
+        /*
+         * I've thought of making a game where you would chain hits against a wall using a ball and a paddle. Longer chains without the ball hitting the floor would count for more points. If you're competing against another player, the person with the most points after the ball hits the floor twice before its hit again would win.
+         * I'm interested in learning more about how to implement the "saving" functionality to your game.
+         * I've also thought of making a card game where you battle others using your own handcrafted deck of cards. You would find more cards throughout the game which you can choose to add to your deck, allowing players to "buildcraft" their deck towards a specific style of play.
+        */
     }
 
     // Update is called once per frame

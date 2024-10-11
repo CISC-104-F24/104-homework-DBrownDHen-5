30b4826fc1b4baefc6c6e743a86e98249fffade3
DBrownDHen <dbrownki@udel.edu>
2024-10-11 16:23:14 -0400

    Completes Parts C and D of Homework 2

--- Homework Project/Assets/Scripts/Movement.cs
+++ Homework Project/Assets/Scripts/Movement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Movement : MonoBehaviour
 {
@@ -10,6 +11,10 @@ public class Movement : MonoBehaviour
     public float normalJumpHeight = 5.0f;
     public float jumpPower;
 
+    public float maxJumpPower = 10f;
+
+    public float jumpCooldown = 2f;
+    public float cooldownTimer = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,8 +25,6 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // bool isVoteKeyPressed = Input.GetKeyDown(voteKey);
-
         Rigidbody objRigidBody = GetComponent<Rigidbody>();
 
         Vector3 movementDirection = Vector3.zero;
@@ -40,11 +43,15 @@ public class Movement : MonoBehaviour
             moveSpeed = normalSpeed;
         }
 
-        if (Input.GetKey(KeyCode.LeftControl))
+        if (Input.GetKey(KeyCode.LeftControl) && !(Input.GetKeyDown(KeyCode.Space)))
         {
-            while (!(Input.GetKey(KeyCode.Space)))
+            if (!(normalJumpHeight + jumpPower >= maxJumpPower))
             {
-                jumpPower += 0.1f;
+                jumpPower += 1.0f * Time.deltaTime;
+            }
+            else
+            {
+                jumpPower = maxJumpPower - normalJumpHeight;
             }
         }
         else
@@ -72,9 +79,27 @@ public class Movement : MonoBehaviour
         transform.position += movementDirection.normalized * moveSpeed * Time.deltaTime;
 
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && cooldownTimer == 0f)
         {
             objRigidBody.AddForce(new Vector3(0f, jumpHeight, 0f), ForceMode.Impulse);
+            cooldownTimer = jumpCooldown;
         }
+
+        if (cooldownTimer != 0f)
+        {
+            if (cooldownTimer < 0f)
+            {
+                cooldownTimer = 0f;
+            }
+            else
+            {
+                cooldownTimer -= Time.deltaTime;
+            }
+        }
+
+        /* FOR PART D, I implemented the following two mechanics:
+         * Add a “maximum jump power” to the charge jump
+         * Put a “cooldown timer” on the character’s jump
+        */
     }
 }

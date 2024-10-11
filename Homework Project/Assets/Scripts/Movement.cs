831a08374428563702e0a8e2ed31e87f536fa595
DBrownDHen <dbrownki@udel.edu>
2024-10-11 14:36:23 -0400

    Emergency Save

--- Homework Project/Assets/Scripts/Movement.cs
+++ Homework Project/Assets/Scripts/Movement.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float moveSpeed = 3.0f;
+    public float sprintSpeed = 5.0f;
+    public float normalSpeed = 3.0f;
+
+    public float normalJumpHeight = 5.0f;
+    public float jumpPower;
 
 
     // Start is called before the first frame update
@@ -21,6 +25,33 @@ public class Movement : MonoBehaviour
         Rigidbody objRigidBody = GetComponent<Rigidbody>();
 
         Vector3 movementDirection = Vector3.zero;
+
+        float moveSpeed;
+
+        float jumpHeight = normalJumpHeight + jumpPower;
+        
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveSpeed = sprintSpeed;
+        }
+        else
+        {
+            moveSpeed = normalSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            while (!(Input.GetKey(KeyCode.Space)))
+            {
+                jumpPower += 0.1f;
+            }
+        }
+        else
+        {
+            jumpPower = 0f;
+        }
+
 
         if (Input.GetKey(KeyCode.W))
         {
@@ -43,7 +74,7 @@ public class Movement : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            objRigidBody.AddForce(new Vector3(0f, 5f, 0f), ForceMode.Impulse);
+            objRigidBody.AddForce(new Vector3(0f, jumpHeight, 0f), ForceMode.Impulse);
         }
     }
 }

75795f2fd14a91485cc466442f2b597708cbde77
DBrownDHen <dbrownki@udel.edu>
2024-11-13 16:34:04 -0500

    Completes Part B of Homework 4

--- Homework Project/Assets/Scripts/Card.cs
+++ Homework Project/Assets/Scripts/Card.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Card : MonoBehaviour
 {
+    public int num;
+    public Color bgColor;
+    public int suit;
+
+    public TextMeshProUGUI topNum;
+    public TextMeshProUGUI bottomNum;
+    public Image background;
+    public Image suitIcon;
+
+
     // Start is called before the first frame update
     void Start()
     {

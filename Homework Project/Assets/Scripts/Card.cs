a65ec415d84322fe16b8ea1dfd1bf78801f27357
DBrownDHen <dbrownki@udel.edu>
2024-11-13 17:08:53 -0500

    Checkpoint Save (I'm paranoid)

--- Homework Project/Assets/Scripts/Card.cs
+++ Homework Project/Assets/Scripts/Card.cs
@@ -6,9 +6,10 @@ using TMPro;
 
 public class Card : MonoBehaviour
 {
-    public int num;
-    public Color bgColor;
-    public int suit;
+    public int num = 1;
+    public Color bgColor = new Color(0.698039216f, 0.698039216f, 0.698039216f);
+    public Color currentColor;
+    public int suit = 1;
 
     public TextMeshProUGUI topNum;
     public TextMeshProUGUI bottomNum;
@@ -19,12 +20,77 @@ public class Card : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        background.color = bgColor;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void changeNumber(int number)
+    {
+        if (number < 0)
+        {
+            if (num != 1)
+            {
+                num += number;
+            }
+        }
+        else
+        {
+            if (num != 99)
+            {
+                num += number;
+            }
+        }
+        topNum.text = num.ToString();
+        bottomNum.text = num.ToString();
+    }
+
+    public void randomizeBackground()
+    {
+        float randomRed = Random.Range(0.0f, 1.0f);
+        float randomGreen = Random.Range(0.0f, 1.0f);
+        float randomBlue = Random.Range(0.0f, 1.0f);
+        currentColor = new Color(randomBlue, randomGreen, randomRed);
+        background.color = currentColor;
+    }
+
+    public void resetBackground()
+    {
+        background.color = bgColor;
+    }
+
+    public void changeSuit()
+    {
+        if (suit != 4)
+        {
+            suit++;
+        }
+        else
+        {
+            suit = 1;
+        }
+
+        // The card's suit will be changed depending on the value stored in "suit" (1, 2, 3, or 4)
+
+        if (suit == 1)
+        {
+            suitIcon.sprite = idk?;
+        }
+        else if (suit == 2)
+        {
+
+        }
+        else if (suit == 3)
+        {
+
+        }
+        else if (suit == 4)
+        {
+
+        }
     }
 }
